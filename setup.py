c9fa6e6bf2c611e25d27adf6f97a1a275611503f
Guy Dumais <guy@private-ai.com>
2023-02-16 14:43:32 -0500

    DEID-1458 - add missing dependency

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     install_requires=[
         "requests==2.28.1",
         "python-dotenv==0.19.0",
+        "unidiff~=0.7.4",
     ],
     keywords=["python", "pre-commit"],
     entry_points={"console_scripts": ["pii_check=pii_check.pii_check_hook:main"]},
